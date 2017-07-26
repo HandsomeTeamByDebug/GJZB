a5e9b6134dd901e196c36a99198ad082a256bb54
lin <1099195068@qq.com>
2017-07-26 13:25:10 +0800

    肖萍

--- GJZB/test/com/lnsf/test/daoTest/EmployeeDaoTest.java
+++ GJZB/test/com/lnsf/test/daoTest/EmployeeDaoTest.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.lnsf.dao.impl.EmployeeDao;
+import com.lnsf.dao.EmployeeDao;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/applicationContext.xml")
