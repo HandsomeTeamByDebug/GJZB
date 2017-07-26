531909a67151254ed7e0b59b3af1f4230189e65a
lin <1099195068@qq.com>
2017-07-26 09:45:49 +0800

    肖萍

---
+++ GJZB/test/com/lnsf/test/daoTest/EmployeeDaoTest.java
@@ -0,0 +1,29 @@
+package com.lnsf.test.daoTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.lnsf.dao.impl.EmployeeDao;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/applicationContext.xml")
+public class EmployeeDaoTest {
+	
+	@Autowired
+	private EmployeeDao employeeDao;
+	
+	@Test
+	public void getMaxEmpno(){
+		try {
+			int i = employeeDao.getMaxEmpno();
+			System.out.println(i);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
